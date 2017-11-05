10af4a2fd05efc80af8c139701be5f549397107e
Bowen Zhong <bz4e14@soton.ac.uk>
2017-11-05 22:49:50 +0800

    isRequire isTopLevelRequire

--- index.js
+++ index.js
@@ -1,9 +1,56 @@
 module.exports = /** @class */ (function () {
     function moduleType() {
+        var _this = this;
+        /**
+         * whether it is the module which has require expression at the top of it.
+         *
+         * example:
+         * module a:
+         * require('a');
+           let a = 123;
+           ...
+           
+           the above 'module a' has top level require expression
+      
+           AST:
+            {
+              "type":"Program",
+                "body":[
+                  {
+                    "type": "ExpressionStatement",
+                    "expression": {
+                      "type": "CallExpression",
+                      "callee": {
+                        "type": "Identifier",
+                        "name": "require"
+                      },
+                      "arguments": [
+                        {
+                          "type": "Literal",
+                          "value": "a",
+                          "raw": "'a'"
+                        }
+                      ]
+                    }
+                  },
+                  Object{...}
+                ],
+                "sourceType":"script"
+            }
+         */
+        this.isTopLevelRequire = function (node) {
+            if (!node ||
+                node.type !== 'Program' ||
+                Object.prototype.toString.call(node.body) !== "[object Array]" ||
+                !node.body[0] ||
+                !node.body[0].expression)
+                return false;
+            return _this.isRequire(node.body[0].expression);
+        };
     }
     /**
      * whether it is AMD define expression
-     * typical AMD define express likes:
+     * example:
      * define(['jquery'] , function ($) {
         return function () {};
        });
@@ -25,6 +72,37 @@ module.exports = /** @class */ (function () {
             node.type === 'CallExpression' &&
             nodeCallee.type === 'Identifier' &&
             nodeCallee.name === 'define';
+    };
+    /**
+     * whether it is commonjs require expression
+     *
+     * example:
+     * require('a');
+     * AST:
+     *
+     * {
+          "type": "CallExpression",
+          "callee": {
+            "type": "Identifier",
+            "name": "require"
+          },
+          "arguments": [
+            {
+              "type": "Literal",
+              "value": "a",
+              "raw": "'a'"
+            }
+          ]
+        }
+     */
+    moduleType.prototype.isRequire = function (node) {
+        if (!node)
+            return false;
+        var nodeCallee = node.callee;
+        return nodeCallee &&
+            node.type === 'CallExpression' &&
+            nodeCallee.type === 'Identifier' &&
+            nodeCallee.name === 'require';
     };
     return moduleType;
 }());
--- index.ts
+++ index.ts
@@ -1,17 +1,8 @@
-interface defineNode {
-  type: string,
-  callee: {
-    type: string,
-    name: string
-  },
-  arguments: Array<object>
-}
-
 module.exports = class moduleType {
 
   /**
    * whether it is AMD define expression
-   * typical AMD define express likes:
+   * example:
    * define(['jquery'] , function ($) {
       return function () {};
      });
@@ -25,15 +16,100 @@ module.exports = class moduleType {
         "arguments":Array[2]
       }
    */
-  isDefine(node: defineNode): boolean {
+  isDefine(node: any): boolean {
     if (!node) return false;
 
     let nodeCallee = node.callee;
 
-    
-    return  nodeCallee &&
+    return nodeCallee &&
       node.type === 'CallExpression' &&
       nodeCallee.type === 'Identifier' &&
       nodeCallee.name === 'define';
   }
+
+  /**
+   * whether it is commonjs require expression
+   * 
+   * example:
+   * require('a');
+   * AST:
+   * 
+   * {
+        "type": "CallExpression",
+        "callee": {
+          "type": "Identifier",
+          "name": "require"
+        },
+        "arguments": [
+          {
+            "type": "Literal",
+            "value": "a",
+            "raw": "'a'"
+          }
+        ]
+      }
+   */
+  isRequire(node: any): boolean {
+    if (!node) return false;
+
+    let nodeCallee = node.callee;
+
+    return nodeCallee &&
+      node.type === 'CallExpression' &&
+      nodeCallee.type === 'Identifier' &&
+      nodeCallee.name === 'require';
+  }
+
+  /**
+   * whether it is the module which has require expression at the top of it.
+   * 
+   * example:
+   * module a:
+   * require('a');
+     let a = 123;
+     ...
+     
+     the above 'module a' has top level require expression
+
+     AST:
+      {
+        "type":"Program",
+          "body":[
+            {
+              "type": "ExpressionStatement",
+              "expression": {
+                "type": "CallExpression",
+                "callee": {
+                  "type": "Identifier",
+                  "name": "require"
+                },
+                "arguments": [
+                  {
+                    "type": "Literal",
+                    "value": "a",
+                    "raw": "'a'"
+                  }
+                ]
+              }
+            },
+            Object{...}
+          ],
+          "sourceType":"script"
+      }
+   */
+  isTopLevelRequire = (node: any): boolean => {
+    if (
+      !node ||
+      node.type !== 'Program' ||
+      Object.prototype.toString.call(node.body) !== "[object Array]" ||
+      !node.body[0] ||
+      !node.body[0].expression
+    ) return false;
+    
+    return this.isRequire(node.body[0].expression);
+  }
+  
 }
+
+
+
