0c0d007186052e5bf29df90dc17697f2fe9ab15f
Bowen Zhong <bz4e14@soton.ac.uk>
2017-11-06 11:16:48 +0800

    some AMD related methods

--- index.js
+++ index.js
@@ -80,6 +80,201 @@ module.exports = /** @class */ (function () {
                 node.arguments[0] &&
                 node.arguments[0].type === 'ArrayExpression';
         };
+        /**
+         * whether it is AMD named form. This method only check whether it has AMD name.
+         *
+         * example:
+         * define('a',['jquery'], function($){})
+         * AST:
+         * {
+            "type":"CallExpression",
+            "callee":{
+              "type":"Identifier",
+              "name":"define"
+            },
+            "arguments":[
+              {
+                "type": "Literal",
+                "value": "a",
+                "raw": "'a'"
+              },
+              Object{...},
+              Object{...}
+            ]
+          }
+         */
+        this.isAMDNamedForm = function (node) {
+            return _this.isDefine &&
+                _this.isArray(node.arguments) &&
+                node.arguments[0] &&
+                node.arguments[0].type === 'Literal';
+        };
+        /**
+         * whether it is AMD dependency form. This method only check whether it has AMD dependencies array.
+         *
+         * example:
+         * define(['jquery'] , function ($) {});
+         * AST:
+         *{
+            "type":"CallExpression",
+            "callee":{
+              "type":"Identifier",
+              "name":"define"
+            },
+            "arguments":[
+              {
+                "type": "ArrayExpression",
+                "elements": [
+                  {
+                    "type": "Literal",
+                    "value": "jquery",
+                    "raw": "'jquery'"
+                  }
+                ]
+              },
+              Object{...}
+            ]
+          }
+         */
+        this.isAMDDependencyForm = function (node) {
+            return _this.isDefine &&
+                _this.isArray(node.arguments) &&
+                node.arguments[0] &&
+                node.arguments[0].type === 'ArrayExpression';
+        };
+        /**
+         * whether it is AMD factory form. This method only check whether it has a function with
+         * 'require' parameter, it does not check the content within function body.
+         *
+         * example:
+         * define(function (require) {});
+         * AST:
+         * {
+            "type":"CallExpression",
+              "callee":{
+              "type":"Identifier",
+              "name":"define"
+            },
+            "arguments":[
+              {
+                "type": "FunctionExpression",
+                "id": null,
+                "params": [
+                  {
+                    "type": "Identifier",
+                    "name": "require"
+                  }
+                ],
+                "body": {
+                  "type": "BlockStatement",
+                  "body": [
+      
+                  ]
+                },
+                "generator": false,
+                "expression": false,
+                "async": false
+              }
+            ]
+          }
+         */
+        this.isAMDFactoryForm = function (node) {
+            return _this.isDefine &&
+                _this.isArray(node.arguments) &&
+                node.arguments[0] &&
+                node.arguments[0].type === 'FunctionExpression' &&
+                _this.isArray(node.arguments[0].params) &&
+                node.arguments[0].params[0] &&
+                node.arguments[0].params[0].type === 'Identifier' &&
+                node.arguments[0].params[0].name === 'require';
+        };
+        /**
+         * whether it is AMD no dependency form.
+         *
+         * example:
+         * define({
+            color: "black",
+            size: "unisize"
+           });
+      
+           AST:
+          {
+            "type":"CallExpression",
+            "callee":{
+              "type":"Identifier",
+              "name":"define"
+            },
+            "arguments":[
+              {
+                "type": "ObjectExpression",
+                "properties": [
+      
+                ]
+              }
+            ]
+          }
+         */
+        this.isAMDNoDependencyForm = function (node) {
+            return _this.isDefine(node) &&
+                _this.isArray(node.arguments) &&
+                node.arguments[0].type === 'ObjectExpression';
+        };
+        /**
+         * whether it is AMD with simplified CommonJS wrapper.
+         *
+         * example:
+         * define(function(require, exports, module) {});
+         * AST:
+         * {
+            "type":"CallExpression",
+            "callee":{
+              "type":"Identifier",
+              "name":"define"
+            },
+            "arguments":[
+              {
+                "type": "FunctionExpression",
+                "id": null,
+                "params": [
+                  {
+                    "type": "Identifier",
+                    "name": "require"
+                  },
+                  {
+                    "type": "Identifier",
+                    "name": "exports"
+                  },
+                  {
+                    "type": "Identifier",
+                    "name": "module"
+                  }
+                ],
+                "body": {
+                  "type": "BlockStatement",
+                  "body": [
+      
+                  ]
+                },
+                "generator": false,
+                "expression": false,
+                "async": false
+              }
+            ]
+          }
+         */
+        this.isAMDCommonJSWrapperForm = function (node) {
+            if (!_this.isDefine(node) ||
+                !_this.isArray(node.arguments) ||
+                !node.arguments[0] ||
+                node.arguments[0].type !== 'FunctionExpression' ||
+                !_this.isArray(node.arguments[0].params) ||
+                node.arguments[0].params.length !== 3)
+                return false;
+            var params = node.arguments[0].params;
+            return params[0].type === 'Identifier' && params[0].name === 'require' &&
+                params[1].type === 'Identifier' && params[1].name === 'exports' &&
+                params[2].type === 'Identifier' && params[2].name === 'module';
+        };
     }
     moduleType.prototype.isArray = function (param) {
         return Object.prototype.toString.call(param) === "[object Array]";
